238474f42ec1c00d077af1745cb89b9a7d111c7a
Erlangga Adinegoro <adinegoro11@gmail.com>
2024-01-24 13:26:11 +0700

    Create API register new user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UserRegisterRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Support\Facades\Hash;
+
+// use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function register(UserRegisterRequest $request): UserResource
+    {
+        $data = $request->validated();
+        if (User::where('username', $data['username'])->count() == 1) {
+            throw new HttpResponseException(response([
+                'errors' => [
+                    'username' => [
+                        'username already registered'
+                    ]
+                ]
+            ], 400));
+        }
+
+        $user = new User($data);
+        $user->passwod = Hash::make($data['password']);
+        $user->save();
+        return new UserResource($user);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
